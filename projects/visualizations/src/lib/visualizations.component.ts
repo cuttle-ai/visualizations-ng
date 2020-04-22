72eecea246fb6516124e6095119dd9954cfff7e0
Melvin Davis <melvinodsa@gmail.com>
2020-04-22 22:57:28 +0530

    Basic angular wrapper

--- projects/visualizations/src/lib/visualizations.component.ts
+++ projects/visualizations/src/lib/visualizations.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import InitVisualizations from '@cuttleai/visualizations';
+import QueryResult from '@cuttleai/visualizations/lib/models/query';
 
 @Component({
   selector: 'lib-visualizations',
   template: `
-    <p>
-      visualizations works!
-    </p>
+    <div id="cuttle-visualization"></div>
   `,
   styles: []
 })
 export class VisualizationsComponent implements OnInit {
 
+  @Input()
+  queryResult: QueryResult;
+
   constructor() { }
 
   ngOnInit() {
+    InitVisualizations('cuttle-visualization', this.queryResult)
   }
 
 }

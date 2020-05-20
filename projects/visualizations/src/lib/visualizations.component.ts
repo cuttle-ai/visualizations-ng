5aedc2f9c6cb9532d1a64c33bb875a76206ee387
Melvin Davis <melvinodsa@gmail.com>
2020-05-21 00:45:23 +0530

    updated the libraries

--- projects/visualizations/src/lib/visualizations.component.ts
+++ projects/visualizations/src/lib/visualizations.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-import InitVisualizations from '@cuttleai/visualizations';
-import QueryResult from '@cuttleai/visualizations/lib/models/query';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
+import InitVisualizations from "@cuttleai/visualizations";
+import QueryResult from "@cuttleai/visualizations/lib/models/query";
 
 @Component({
-  selector: 'lib-visualizations',
-  template: `
-    <div id="cuttle-visualization"></div>
-  `,
-  styles: []
+  selector: "lib-visualizations",
+  template: ` <div id="cuttle-visualization"></div> `,
+  styles: [],
 })
 export class VisualizationsComponent implements OnInit, OnChanges {
-
   @Input()
   queryResult: QueryResult;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   ngOnChanges(changes: SimpleChanges) {
-    InitVisualizations('cuttle-visualization', changes.queryResult.currentValue);
+    InitVisualizations(
+      "cuttle-visualization",
+      changes.queryResult.currentValue
+    );
   }
-
 }
